eb6129677c706e2eed213e5306cefbf6c7c78348
LRJ <1330234814@qq.com>
2021-03-10 07:59:50 +0800

    good

--- NumberSeven.py
+++ NumberSeven.py
@@ -1,4 +1,10 @@
 import PyPDF2
 import openpyxl
 
-
+fn = 'sales_001.xlsx'
+wb = openpyxl.load_workbook(fn, data_only=True)
+ws = wb.get_sheet_by_name('全国订单明细')
+for cell in list(ws.columns)[0]:
+    print(cell.value)
+for cell in list(ws.rows)[1]:
+    print(cell.value,end = ' ')
